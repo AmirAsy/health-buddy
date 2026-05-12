c3bd5e34f6664ccbd81552ed96638983fc00f885
AmirAsy <amir.asyrani@gmail.com>
2026-05-12 15:24:00 +0800

    Seperating the Back to top function JS

--- JS/health-tips.js
+++ JS/health-tips.js
@@ -162,29 +162,3 @@ function setActiveButton(activeButton) {
 
   activeButton.classList.add("active");
 }
-
-// Back To Top Button
-const backToTopBtn =
-  document.getElementById("backToTopBtn");
-
-// Show button when scrolling
-window.addEventListener("scroll", () => {
-
-  if (window.scrollY > 300) {
-    backToTopBtn.style.display = "block";
-  }
-  else {
-    backToTopBtn.style.display = "none";
-  }
-
-});
-
-// Scroll to top
-backToTopBtn.addEventListener("click", () => {
-
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
-
-});
