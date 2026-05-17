1eba4f7e898235c89b4ff2ba881c29b31b173025
AmirAsy <amir.asyrani@gmail.com>
2026-05-17 11:50:22 +0800

    Add Mobile Responsive

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,5 +1,22 @@
 fetch("navbar.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("navbar").innerHTML = data;
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("navbar").innerHTML = data;
+
+    const hamburgerBtn =
+      document.getElementById("hamburgerBtn");
+
+    const navMenu =
+      document.getElementById("navMenu");
+
+    hamburgerBtn.addEventListener("click", () => {
+      navMenu.classList.toggle("active");
+
+      if (navMenu.classList.contains("active")) {
+        hamburgerBtn.textContent = "✕";
+      }
+      else {
+        hamburgerBtn.textContent = "☰";
+      }
     });
+  });
