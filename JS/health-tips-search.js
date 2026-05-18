3c9ac5627310346acaa5ad565469104f5e2182d4
AmirAsy <amir.asyrani@gmail.com>
2026-05-18 18:55:31 +0800

    Add Sorting Function

--- JS/health-tips-search.js
+++ JS/health-tips-search.js
@@ -1,11 +1,51 @@
-function searchTips(e) {
-  const keyword = e.target.value.toLowerCase();
+const searchInput =
+  document.getElementById("search");
+const sortSelect =
+  document.getElementById("sort-select");
 
-  const filtered = allTips.filter(tip =>
+/* ---------- SEARCH EVENT ---------- */
+
+searchInput.addEventListener("input", applyFiltersAndSort);
+
+/* ---------- SORT EVENT ---------- */
+
+sortSelect.addEventListener("change", applyFiltersAndSort);
+
+/* ---------- MAIN FUNCTION ---------- */
+
+function applyFiltersAndSort() {
+
+  const keyword =
+    searchInput.value.toLowerCase();
+
+  /* ---------- FILTER ---------- */
+
+  let filtered = allTips.filter(tip =>
+
     tip.title.toLowerCase().includes(keyword) ||
     tip.category.toLowerCase().includes(keyword) ||
     tip.content.toLowerCase().includes(keyword)
+
   );
 
+  /* ---------- SORTING ---------- */
+
+  if (sortSelect.value === "az") {
+    filtered.sort((a, b) =>
+      a.title.localeCompare(b.title)
+    );
+
+  }
+
+  else if (sortSelect.value === "za") {
+    filtered.sort((a, b) =>
+      b.title.localeCompare(a.title)
+    );
+
+  }
+
+  /* ---------- RENDER ---------- */
+
   renderTips(filtered);
+
 }
